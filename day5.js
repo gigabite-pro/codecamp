a4e82bc4f3a5b4e98c968f103edd70cd3c910c44
Vaibhav Sharma <rajmachawal19@gmail.com>
2022-07-11 21:07:42 +0530

    day 5

---
+++ day5.js
@@ -0,0 +1,50 @@
+console.log('connected')
+
+// let user = 'This is a sentence'
+// console.log(user.length)
+// console.log(user.split(' '))
+
+// let number = [1,2,3,4,5,6,7,8,9,10]
+
+// console.log(number[9])
+
+// number.forEach(element => {
+//     console.log(element)
+// })
+
+// let myValues = {
+//     name: 'John',
+//     age: 30,
+//     isMarried: false,
+// }
+
+
+// console.log(typeof(myValues))
+
+// let stringifiedObject = JSON.stringify(myValues)
+// console.log(stringifiedObject)
+// console.log(typeof(stringifiedObject))
+
+// let parsedObject = JSON.parse(stringifiedObject)
+// console.log(parsedObject)
+// console.log(typeof(parsedObject))
+
+// Object.keys(myValues).forEach(element => {
+//     console.log(myValues[element])
+// });
+
+let a = 3
+let b = 5
+let operation = 'ajfbsdjfb'
+
+if(operation == 'add'){
+    console.log(a + b)
+}else if(operation == 'subtract'){
+    console.log(a-b)
+} else if(operation == 'multiply'){
+    console.log(a*b)
+}else if(operation == 'divide'){
+    console.log(a/b)
+}else{
+    console.log('Invalid operation')
+}
