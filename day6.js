1c8acbebaf3ffd70f5b671606f110614406a8c20
Vaibhav Sharma <rajmachawal19@gmail.com>
2022-07-12 20:58:47 +0530

    day 6

---
+++ day6.js
@@ -0,0 +1,65 @@
+console.log("connected");
+
+// if(number == 5){
+//     console.log('number is 5')
+// }else if(number >= 5){
+//     console.log('number is greater than 5')
+// }else if(number <= 5){
+//     console.log('number is less than 5')
+// }else{
+//     console.log('number is not 5')
+// }
+
+//print table of 5 using for
+// for(let i = 50; i > 4; i = i - 5){
+//     console.log(i)
+// }
+
+//crash browser (infinite)
+// for(i = 1; i < 2; i){
+//     console.log(i)
+// }
+
+//while
+
+// let i = true
+// while(i){
+//     let name = prompt('Enter your name')
+//     if(name == 'admin'){
+//         console.log('Hello admin')
+//     }else{
+//         alert('loop ends')
+//         i = false
+//     }
+// }
+
+//do while
+// let i = 0
+// do{
+//     console.log(i)
+//     i++
+// }while(i < 10)
+
+//for in loop for object keys
+// let obj = {
+//     'name': 'Vaibhav',
+//     'age': 20,
+//     'city': 'Gurgaon'
+// }
+
+// for(let key in obj){
+//     console.log(key, ':' , obj[key])
+// }
+
+//for in loop for array index
+// let array = ["a", "b", "c", "d", "e"];
+
+// for (let index in array) {
+//     console.log(array[index]);
+// }
+
+//for of loop for array
+// let array = ["a", "b", "c", "d", "e"];
+// for(letters of array){
+//     console.log(letters + ' is a letter of english alphabets');
+// }
